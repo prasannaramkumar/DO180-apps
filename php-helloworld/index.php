a6864df6ddfa0b88326af51573d0219b2059a18e
Student User <student@workstation.lab.example.com>
2023-03-16 10:56:42 -0400

    redhatdemo_fix

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,5 +1,5 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "I have changed it and and changed made by Prasannaramkumar" :wq
-	:wq
+print "I have changed it and and changed made by Prasannaramkumar";
+
 ?>
